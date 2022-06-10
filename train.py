88461a2d887ef39f0df2106771c1a32074f5df92
Sooyyoungg <rlatndud0513@ewhain.net>
2022-06-10 16:14:39 +0900

    train

--- train.py
+++ train.py
@@ -16,13 +16,15 @@ def main():
 
     ## Data Loader
     train_list = pd.read_csv(config.train_list)
+    valid_list = pd.read_csv(config.valid_list)
 
-    # train_data = DataSplit(data_list=config.train_list, data_root=config.train_root)
+    train_data = DataSplit(data_list=config.train_list, data_root=config.train_root)
     valid_data = DataSplit(data_list=config.valid_list, data_root=config.valid_root)
 
-    # data_loader_train = torch.utils.data.DataLoader(train_data, batch_size=config.batch_size, shuffle=True, num_workers=16, pin_memory=False)
-    data_loader_train = torch.utils.data.DataLoader(valid_data, batch_size=config.batch_size, shuffle=True, num_workers=16, pin_memory=False)
-    print(len(data_loader_train), "x", config.batch_size,"(batch size) =", len(train_list))
+    data_loader_train = torch.utils.data.DataLoader(train_data, batch_size=config.batch_size, shuffle=True, num_workers=16, pin_memory=False)
+    data_loader_valid = torch.utils.data.DataLoader(valid_data, batch_size=1, shuffle=True, num_workers=16, pin_memory=False)
+    print("Train: ", len(data_loader_train), "x", config.batch_size,"(batch size) =", len(train_list))
+    print("Valid: ", len(data_loader_valid), "x", config.batch_size,"(batch size) =", len(valid_list))
 
     ## Start Training
     model = Pix2Pix(config)
@@ -59,26 +61,33 @@ def main():
             rmse = torch.sqrt(mse / config.batch_size)
 
             # save & print loss values
-            train_writer.add_scalar('Loss_G', train_dict['G_loss'])
-            train_writer.add_scalar('Loss_D', train_dict['D_loss'])
+            train_writer.add_scalar('Loss_G', train_dict['G_loss'], tot_itr)
+            train_writer.add_scalar('Loss_D', train_dict['D_loss'], tot_itr)
             print("Epoch: %d/%d | itr: %d/%d | tot_itrs: %d | Loss_G: %.9f | Loss_D: %.9f | RMSE: %.9f"%(epoch+1, config.n_epoch, i+1, itr_per_epoch, tot_itr, train_dict['G_loss'], train_dict['D_loss'], rmse))
 
         valid_G_loss = 0
         valid_D_loss = 0
+        valid_mse = 0
         v = 0
-        # for v, v_data in enumerate(data_loader_valid):
-        #     val_dict = model.val(v_data)
-        #     valid_G_loss += val_dict['G_loss']
-        #     valid_D_loss += val_dict['D_loss']
-        # v_G_avg_loss = float(valid_G_loss / (v+1))
-        # v_D_avg_loss = float(valid_D_loss / (v+1))
-        # print("===> Validation <=== Epoch[%d/%d] | Loss_G: %.9f | Loss_D: %.9f".format(epoch, config.n_epoch, v_G_avg_loss, v_D_avg_loss))
+        for v, v_data in enumerate(data_loader_valid):
+            val_dict = model.val(v_data)
+            valid_G_loss += val_dict['G_loss']
+            valid_D_loss += val_dict['D_loss']
+            # mse
+            v_diff = val_dict['fake_depth'] - val_dict['real_depth']
+            valid_mse += torch.pow(v_diff, 2)
+        v_G_avg_loss = float(valid_G_loss / (v+1))
+        v_D_avg_loss = float(valid_D_loss / (v+1))
+        valid_rmse = torch.sqrt(valid_mse / config.batch_size)
+        print("===> Validation <=== Epoch[%d/%d] | Loss_G: %.9f | Loss_D: %.9f | RMSE: %.9f".format(epoch, config.n_epoch, v_G_avg_loss, v_D_avg_loss, valid_rmse))
 
         networks.update_learning_rate(model.G_scheduler, model.optimizer_G)
         networks.update_learning_rate(model.D_scheduler, model.optimizer_D)
 
-        # if epoch % 10 == 0:
-        #     save model
+        if epoch % 10 == 0:
+            torch.save(model.state_dict(), config.log_dir)
+            with open(config.log_dir+'/latest_log.txt', 'w') as f:
+                f.writelines('%d, %d'%(epoch, tot_itr))
 
 if __name__ == '__main__':
     main()
